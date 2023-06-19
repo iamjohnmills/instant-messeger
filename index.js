805951aded72e1875823e47865ef26207bde7241
John <john@Johns-MacBook-Pro.local>
2023-06-19 17:33:14 -0500

    adjusted reconnect logic

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
   server.events.on('onSocketMessage', ({ client, event }) => {
     if(event.connect){
-      createSocketUser({ client: client, user_name: event.user_name, room_name: event.room_name })
+      createSocketUser({ client: client, reconnect: event.reconnect, user_name: event.user_name, room_name: event.room_name })
     } else if(event.chat) {
       sendReceiveMessage({ client: client, message: event.message });
     } else if(event.buddylist) {
@@ -33,7 +33,7 @@
     }
   }
 
-  const createSocketUser = ({ client, user_name, room_name }) => {
+  const createSocketUser = ({ client, reconnect, user_name, room_name }) => {
     try {
       if(user_name.length > 20) throw 'Username too long.'
       if(room_name.length > 20) throw 'Room name too long.'
@@ -42,12 +42,12 @@
       const previous_name = client.user_name && client.room_name === room_name ? client.user_name : null;
       client.user_name = user_name.trim();
       client.room_name = room_name.trim();
-      client.send(JSON.stringify({ success: true, connect: true, data: { user_name: user_name, room_name: room_name } }));
+      client.send(JSON.stringify({ success: true, connect: true, reconnect: reconnect, data: { user_name: user_name, room_name: room_name } }));
       const buddies = server.getSocketClientsByRoom({ room_name: room_name }).forEach(buddy => {
         if(previous_name){
           buddy.send(JSON.stringify({ success: true, name_change: true, data: { from: previous_name, to: user_name } }));
         } else {
-          buddy.send(JSON.stringify({ success: true, buddy_join: true, data: { user_name: user_name } }));
+          buddy.send(JSON.stringify({ success: true, buddy_join: true, reconnect: reconnect, data: { user_name: user_name } }));
         }
       });
 
